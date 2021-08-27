78821cf344b2298792e2d518e46b176e2c41b713
Marcelo S. Macorin <ms.macorin2@gmail.com>
2021-08-27 17:54:50 -0300

    adicionando equals e hashcode

--- src/main/java/br/com/macorin/libs/commons_entity/BaseEntity.java
+++ src/main/java/br/com/macorin/libs/commons_entity/BaseEntity.java
@@ -47,4 +47,19 @@ public class BaseEntity {
     public void setUpdated(Date updated) {
         this.updated = updated;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
